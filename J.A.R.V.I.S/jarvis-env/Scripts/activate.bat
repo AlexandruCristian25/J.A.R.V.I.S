3e084d3ec534f17cd9546632d4b5f3af3b71f3bd
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-08-10 17:27:25 +0300

    Add files via upload

--- J.A.R.V.I.S/jarvis-env/Scripts/activate.bat
+++ J.A.R.V.I.S/jarvis-env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Alexandru Cristian\Desktop\J.A.R.V.I.S\jarvis-env"
+set "VIRTUAL_ENV=E:\Stick2\J.A.R.V.I.S\jarvis-env"
 
 if not defined PROMPT set PROMPT=$P$G
 
--- J.A.R.V.I.S/jarvis-env/Scripts/activate.fish
+++ J.A.R.V.I.S/jarvis-env/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\Alexandru Cristian\Desktop\J.A.R.V.I.S\jarvis-env'
+set -gx VIRTUAL_ENV 'E:\Stick2\J.A.R.V.I.S\jarvis-env'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
